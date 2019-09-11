6a04ea6309402debbdd22a3c1c7ccf272a8c7c2f
Agustin Carames <agustin.carames@creativecoefficient.net>
2019-09-11 16:23:35 -0300

    Ignore YellowBox warning from SafeArea

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { createAppContainer } from "react-navigation";
 
 import AppNavigator from "./presentation/AppNavigator";
+import { YellowBox } from "react-native";
+
+YellowBox.ignoreWarnings([
+	"Warning: componentWillReceiveProps is deprecated and will be removed in the next major version." // External error in SafeArea
+]);
 
 const AppContainer = createAppContainer(AppNavigator);
 
