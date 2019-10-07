116594f5a802d9f4a84fa3ac38b36deb64ba1136
Agustin Carames <agustin.carames@creativecoefficient.net>
2019-10-07 14:34:56 -0300

    Add fast QR decode error path

--- src/src/uPort/parseJWT.ts
+++ src/src/uPort/parseJWT.ts
@@ -1,12 +1,13 @@
 import * as t from "io-ts";
-import { left } from "fp-ts/lib/Either";
+import * as Either from "fp-ts/lib/Either";
+import JWTDecode from "jwt-decode";
 
 import { verifyJWT } from "did-jwt";
 import { Resolver } from "did-resolver";
 import { getResolver } from "ethr-did-resolver";
 
-import { SelectiveDisclosureRequestCodec } from "./SelectiveDisclosureRequest";
-import { VerifiedClaimCodec } from "./VerifiedClaim";
+import { SelectiveDisclosureRequestCodec, SelectiveDisclosureRequest } from "./SelectiveDisclosureRequest";
+import { VerifiedClaimCodec, VerifiedClaim } from "./VerifiedClaim";
 
 // This is required by verifyJWT
 if (typeof Buffer === "undefined") {
@@ -20,11 +21,18 @@ const resolver = new Resolver(ethrDidResolver);
 
 const JWTCodec = t.union([SelectiveDisclosureRequestCodec, VerifiedClaimCodec]);
 
-export default async function parseJWT(jwt: string) {
+export default async function parseJWT(
+	jwt: string
+): Promise<Either.Either<any, SelectiveDisclosureRequest | VerifiedClaim>> {
 	try {
+		const unverifiedContent = JWTCodec.decode(JWTDecode(jwt));
+		if (Either.isLeft(unverifiedContent)) {
+			return unverifiedContent;
+		}
+
 		const { payload } = await verifyJWT(jwt, { resolver });
 		return JWTCodec.decode(payload);
 	} catch (e) {
-		return left(e);
+		return Either.left(e);
 	}
 }
