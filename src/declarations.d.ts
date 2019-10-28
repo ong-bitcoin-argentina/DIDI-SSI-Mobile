293a57f95e12d7627a527029f64c624e04281630
Agustin Carames <agustin.carames@creativecoefficient.net>
2019-10-28 16:55:11 -0300

    Extract RNUportHDSigner type declarations

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -9,49 +9,5 @@ declare module "react-navigation-header-buttons/src/overflowMenuPressHandlers" {
 	export function defaultOnOverflowMenuPress(params: onOverflowMenuPressParams): void;
 }
 
-declare module "react-native-uport-signer" {
-	type KeyAddress = string;
-	type SeedPhrase = string;
-	type DerivationPath = string;
-	type ProtectionLevel = "simple" | "prompt" | "singleprompt";
-	type Base64EncodedJWT = string;
-
-	interface AddressObject {
-		address: KeyAddress;
-		pubKey: string;
-	}
-	interface ExpandedJWT {
-		v: number;
-		r: string;
-		s: string;
-	}
-
-	interface RNUportHDSignerType {
-		hasSeed(): Promise<boolean>;
-		listSeedAddresses(): Promise<Array<KeyAddress>>;
-
-		createSeed(protection: ProtectionLevel): Promise<AddressObject>;
-		importSeed(phrase: SeedPhrase, protection: ProtectionLevel): Promise<AddressObject>;
-		deleteSeed(address: KeyAddress): void;
-		showSeed(address: KeyAddress, explanation: string): Promise<SeedPhrase>;
-
-		signJwt(
-			address: KeyAddress,
-			path: DerivationPath,
-			jwt: Base64EncodedJWT,
-			explanation: string
-		): Promise<ExpandedJWT>;
-		signEthereumTransaction(
-			address: KeyAddress,
-			path: DerivationPath,
-			jwt: Base64EncodedJWT,
-			explanation: string
-		): Promise<ExpandedJWT>;
-
-		UPORT_ROOT_DERIVATION_PATH: DerivationPath;
-	}
-	export const RNUportHDSigner: RNUportHDSignerType;
-}
-
 declare module "ethr-did-resolver";
 declare module "redux-persist-fs-storage";
