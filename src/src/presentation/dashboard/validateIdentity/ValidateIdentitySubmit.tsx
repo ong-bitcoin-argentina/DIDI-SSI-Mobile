d251afed8378a8e1aa6131e28c6389bf1afcdd1c
Agustin Carames <agustin.carames@creativecoefficient.net>
2019-12-09 15:40:24 -0300

    Add renaper service call

--- src/src/presentation/dashboard/validateIdentity/ValidateIdentitySubmit.tsx
+++ src/src/presentation/dashboard/validateIdentity/ValidateIdentitySubmit.tsx
@@ -1,29 +1,51 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
 import { TakePictureResponse } from "react-native-camera/types";
+import { readFile } from "react-native-fs";
 
 import { DidiScreen } from "../../common/DidiScreen";
 import NavigationHeaderStyle from "../../common/NavigationHeaderStyle";
+import { ServiceObserver } from "../../common/ServiceObserver";
 import commonStyles from "../../resources/commonStyles";
-import DidiButton from "../../util/DidiButton";
+import { DidiServiceButton } from "../../util/DidiServiceButton";
 import { DidiText } from "../../util/DidiText";
 import NavigationEnabledComponent from "../../util/NavigationEnabledComponent";
 
 import { DocumentBarcodeData } from "../../../model/DocumentBarcodeData";
+import { isPendingService } from "../../../services/ServiceStateStore";
+import { validateDni } from "../../../services/user/validateDni";
+import { didiConnect } from "../../../store/store";
 import strings from "../../resources/strings";
 
-export interface ValidateIdentitySubmitNavigation {
-	DashboardRoot: {};
-}
 export interface ValidateIdentitySubmitProps {
 	documentData: DocumentBarcodeData;
 	front: TakePictureResponse;
 	back: TakePictureResponse;
 	selfie: TakePictureResponse;
 }
+interface ValidateIdentitySubmitStateProps {
+	validateIdentityPending: boolean;
+}
+interface ValidateIdentitySubmitDispatchProps {
+	validateIdentity: (
+		barcodeData: DocumentBarcodeData,
+		front: TakePictureResponse,
+		back: TakePictureResponse,
+		selfie: TakePictureResponse
+	) => void;
+}
+type ValidateIdentitySubmitInnerProps = ValidateIdentitySubmitProps &
+	ValidateIdentitySubmitStateProps &
+	ValidateIdentitySubmitDispatchProps;
 
-export class ValidateIdentitySubmitScreen extends NavigationEnabledComponent<
-	ValidateIdentitySubmitProps,
+export interface ValidateIdentitySubmitNavigation {
+	DashboardRoot: {};
+}
+
+const serviceKey = "ValidateIdentity";
+
+class ValidateIdentitySubmitScreen extends NavigationEnabledComponent<
+	ValidateIdentitySubmitInnerProps,
 	{},
 	ValidateIdentitySubmitNavigation
 > {
@@ -40,23 +62,53 @@ export class ValidateIdentitySubmitScreen extends NavigationEnabledComponent<
 				<View>
 					<Text>DNI: {this.props.documentData.dni}</Text>
 					<Text>Genero: {this.props.documentData.gender}</Text>
-					<Text>Numero de Tramite: {this.props.documentData.tramitId}</Text>
+					<Text>Nombre(s): {this.props.documentData.firstNames}</Text>
+					<Text>Apellido(s): {this.props.documentData.lastNames}</Text>
+					<Text>Fecha de Nacimiento: {this.props.documentData.birthDate}</Text>
+					<Text>Numero de tramite: {this.props.documentData.tramitId}</Text>
 				</View>
 
 				<View style={{ justifyContent: "space-around", flexDirection: "row" }}>
-					<Image style={commonStyles.image.image} width={100} height={100} source={{ uri: this.props.front.uri }} />
-					<Image style={commonStyles.image.image} width={100} height={100} source={{ uri: this.props.back.uri }} />
+					<Image style={commonStyles.image.image} width={160} height={100} source={{ uri: this.props.front.uri }} />
+					<Image style={commonStyles.image.image} width={160} height={100} source={{ uri: this.props.back.uri }} />
 				</View>
-				<Image style={commonStyles.image.image} width={100} height={100} source={{ uri: this.props.selfie.uri }} />
+				<Image style={commonStyles.image.image} width={100} height={120} source={{ uri: this.props.selfie.uri }} />
 
 				<DidiText.ValidateIdentity.Reminder>
 					{strings.validateIdentity.submit.reminder}
 				</DidiText.ValidateIdentity.Reminder>
-				<DidiButton
+
+				<ServiceObserver serviceKey={serviceKey} onSuccess={() => this.navigate("DashboardRoot", {})} />
+				<DidiServiceButton
 					title={strings.validateIdentity.submit.buttonText}
-					onPress={() => this.navigate("DashboardRoot", {})}
+					onPress={() =>
+						this.props.validateIdentity(this.props.documentData, this.props.front, this.props.back, this.props.selfie)
+					}
+					isPending={this.props.validateIdentityPending}
 				/>
 			</DidiScreen>
 		);
 	}
 }
+
+const connected = didiConnect(
+	ValidateIdentitySubmitScreen,
+	(state): ValidateIdentitySubmitStateProps => ({
+		validateIdentityPending: isPendingService(state.serviceCalls[serviceKey])
+	}),
+	(dispatch): ValidateIdentitySubmitDispatchProps => ({
+		validateIdentity: async (
+			barcodeData: DocumentBarcodeData,
+			front: TakePictureResponse,
+			back: TakePictureResponse,
+			selfie: TakePictureResponse
+		) => {
+			const frontImage = await readFile(front.uri, "base64");
+			const backImage = await readFile(back.uri, "base64");
+			const selfieImage = await readFile(selfie.uri, "base64");
+			dispatch(validateDni(serviceKey, barcodeData, frontImage, backImage, selfieImage));
+		}
+	})
+);
+
+export { connected as ValidateIdentitySubmitScreen };
---
+++ src/src/services/user/validateDni.ts
@@ -0,0 +1,83 @@
+import { Either } from "fp-ts/lib/Either";
+import * as t from "io-ts";
+
+import { buildComponentServiceCall, serviceCallSuccess } from "../common/componentServiceCall";
+import { ErrorData } from "../common/ErrorData";
+
+import { DocumentBarcodeData } from "../../model/DocumentBarcodeData";
+import { EthrDID } from "../../uPort/types/EthrDID";
+import { getState } from "../internal/getState";
+import { withExistingDid } from "../internal/withExistingDid";
+
+import { commonUserRequest } from "./userServiceCommon";
+
+export interface ValidateDniArguments {
+	baseUrl: string;
+
+	did: EthrDID;
+
+	dni: string;
+	gender: string;
+	firstName: string;
+	lastName: string;
+	birthDate: string;
+	order: string;
+
+	front: string;
+	back: string;
+	selfie: string;
+}
+
+const responseCodec = t.array(t.string);
+
+async function doValidateDni(args: ValidateDniArguments): Promise<Either<ErrorData, string[]>> {
+	return await commonUserRequest(
+		`${args.baseUrl}/validateDni`,
+		{
+			did: args.did.did(),
+			dni: args.dni,
+			gender: args.gender,
+			name: args.firstName,
+			lastName: args.lastName,
+			birthDate: args.birthDate,
+			selfieImage: args.selfie,
+			frontImage: args.front,
+			backImage: args.back
+		},
+		responseCodec
+	);
+}
+
+const validateDniComponent = buildComponentServiceCall(doValidateDni);
+
+export function validateDni(
+	serviceKey: string,
+	barcodeData: DocumentBarcodeData,
+	front: string,
+	back: string,
+	selfie: string
+) {
+	return getState(serviceKey, {}, store => {
+		const baseUrl = store.serviceSettings.didiUserServer;
+		return withExistingDid(serviceKey, {}, did => {
+			const args: ValidateDniArguments = {
+				baseUrl: "http://192.168.2.117:12345",
+				did,
+
+				dni: barcodeData.dni,
+				gender: barcodeData.gender,
+				firstName: barcodeData.firstNames,
+				lastName: barcodeData.lastNames,
+				birthDate: barcodeData.birthDate,
+				order: barcodeData.tramitId,
+
+				front,
+				back,
+				selfie
+			};
+			return validateDniComponent(serviceKey, args, () => {
+				return serviceCallSuccess(serviceKey);
+			});
+		});
+	});
+}
