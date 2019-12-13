49c8ac969d84174195ce0335507a78583573ee9a
Agustin Carames <agustin.carames@creativecoefficient.net>
2019-12-13 14:53:43 -0300

    Hide micro-credentials from credential lists

--- src/src/store/selector/credentialSelector.ts
+++ src/src/store/selector/credentialSelector.ts
@@ -6,6 +6,11 @@ import { CredentialDocument } from "../../model/CredentialDocument";
 
 import { parsedTokenSelector } from "./parsedTokenSelector";
 
-export const credentialSelector = createSelector(parsedTokenSelector, tokens =>
+const allCredentialSelector = createSelector(parsedTokenSelector, tokens =>
 	TypedArray.flatMap(tokens, (tk): CredentialDocument | null => (tk.type === "CredentialDocument" ? tk : null))
 );
+
+export const credentialSelector = createSelector(allCredentialSelector, credentials => {
+	const nested = credentials.map(c => c.nested).reduce((acc, next) => acc.concat(next), []);
+	return credentials.filter(credential => !nested.find(nest => nest.jwt === credential.jwt));
+});
