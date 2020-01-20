687e5e903819458bf532ab2beaaebbeca447dbf8
Agustin Carames <agustin.carames@creativecoefficient.net>
2020-01-20 17:09:13 -0300

    Extract sdk to its own repo

--- sdk/src/util/JSON.ts
+++
@@ -1,7 +0,0 @@
-export type JSONValue = string | number | boolean | null | JSONObject | JSONArray;
-
-export interface JSONObject {
-	[property: string]: JSONValue;
-}
-
-export interface JSONArray extends Array<JSONValue> {}
