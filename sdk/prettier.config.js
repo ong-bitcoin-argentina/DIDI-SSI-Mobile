9e9038aedd91aa601612fb9d9d555bbac4e72935
Agustin Carames <agustin.carames@creativecoefficient.net>
2020-01-20 17:09:13 -0300

    Extract sdk to its own repo

--- sdk/prettier.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	useTabs: true,
-	printWidth: 120
-};
--- src/prepareSdkTarball.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# If the sdk is used locally from its directory, npm will create a symlink, which the metro bundler can't handle.
-# Instead, go indirectly through a tarball install
-
-pushd ../sdk
-npm pack
-popd
-rm -rf node_modules/didi-sdk
