d56d0abbb2f3e5ccf600e4d2f7c16388a377ece9
Agustin Carames <agustin.carames@creativecoefficient.net>
2020-01-24 19:54:17 -0300

    Sort shared credentials below others

--- src/src/store/selector/credentialSelector.ts
+++ src/src/store/selector/credentialSelector.ts
@@ -9,21 +9,31 @@ const allCredentialSelector = createSelector(parsedTokenSelector, tokens =>
 	TypedArray.flatMap(tokens, (tk): CredentialDocument | null => (tk.type === "CredentialDocument" ? tk : null))
 );
 
-export const toplevelCredentialSelector = createSelector(allCredentialSelector, credentials => {
-	const nested = credentials.map(c => c.nested).reduce((acc, next) => acc.concat(next), []);
-	const res = credentials.filter(credential => !nested.find(nest => nest.jwt === credential.jwt));
-	return res.sort((l, r) => {
-		if (l.issuedAt) {
-			if (r.issuedAt) {
-				return r.issuedAt - l.issuedAt;
+export const toplevelCredentialSelector = createSelector(
+	allCredentialSelector,
+	st => st.persisted.did,
+	(credentials, did) => {
+		const nested = credentials.map(c => c.nested).reduce((acc, next) => acc.concat(next), []);
+		const res = credentials.filter(credential => !nested.find(nest => nest.jwt === credential.jwt));
+		return res.sort((l, r) => {
+			if (l.subject.did() !== r.subject.did()) {
+				if (l.subject.did() === did?.did()) {
+					return -1;
+				} else {
+					return +1;
+				}
+			} else if (l.issuedAt) {
+				if (r.issuedAt) {
+					return r.issuedAt - l.issuedAt;
+				} else {
+					return +1;
+				}
 			} else {
-				return +1;
+				return -1;
 			}
-		} else {
-			return -1;
-		}
-	});
-});
+		});
+	}
+);
 
 export type SpecialCredentialMap = Partial<Record<NonNullable<SpecialCredentialFlag>["type"], CredentialDocument>>;
 
