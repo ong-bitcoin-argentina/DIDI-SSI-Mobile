ebe6af955614687f97c50e7bfd36302b16fbcfa5
Agustin Carames <agustin.carames@creativecoefficient.net>
2020-01-27 18:11:55 -0300

    Fix type error

--- src/src/store/selector/combinedRecentActivitySelector.ts
+++ src/src/store/selector/combinedRecentActivitySelector.ts
@@ -8,12 +8,12 @@ export const combinedRecentActivitySelector = createSelector(
 	st => st.persisted.did,
 	toplevelCredentialSelector,
 	st => st.persisted.recentActivity,
-	(did, credentials, recentActivity) => {
+	(did, credentials, recentActivity): RecentActivity[] => {
 		const derived = credentials
 			.filter(doc => doc.subject.did() === did?.did?.())
 			.map(doc => ({
 				...RecentActivity.from("CREATE", [doc]),
-				...(doc.issuedAt ? { date: new Date(doc.issuedAt * 1000) } : {})
+				...(doc.issuedAt ? { date: new Date(doc.issuedAt * 1000).toISOString() } : {})
 			}));
 		return [...derived, ...recentActivity].sort((l, r) => (l.date > r.date ? -1 : 1));
 	}
