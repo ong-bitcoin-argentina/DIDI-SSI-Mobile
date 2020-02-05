869ddfa05ffa5bd52ab88f940a53f2facd855133
Agustin Carames <agustin.carames@creativecoefficient.net>
2020-02-05 18:46:52 -0300

    [107] Hide other people's credentials from share

--- src/src/store/selector/credentialSelector.ts
+++ src/src/store/selector/credentialSelector.ts
@@ -1,4 +1,4 @@
-import { CredentialDocument, SpecialCredentialFlag } from "didi-sdk";
+import { CredentialDocument, EthrDID, SpecialCredentialFlag } from "didi-sdk";
 import { createSelector } from "reselect";
 
 import TypedArray from "../../util/TypedArray";
@@ -15,6 +15,20 @@ export const toplevelCredentialSelector = createSelector(
 	(credentials, did) => {
 		const nested = credentials.map(c => c.nested).reduce((acc, next) => acc.concat(next), []);
 		const res = credentials.filter(credential => !nested.find(nest => nest.jwt === credential.jwt));
+
+		res.push({
+			data: {
+				"1": "1"
+			},
+			issuer: EthrDID.fromKeyAddress("0x0123456789012345678901234567890123456789"),
+			jwt: "1223rqwe",
+			nested,
+			subject: EthrDID.fromKeyAddress("0x1234567890123456789012345678901234567890"),
+			title: "Share",
+			type: "CredentialDocument",
+			category: "finance"
+		});
+
 		return res.sort((l, r) => {
 			if (l.subject.did() !== r.subject.did()) {
 				if (l.subject.did() === did?.did?.()) {
