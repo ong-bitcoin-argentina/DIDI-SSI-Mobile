7ca08a5d5b2e2094a1b8de46c75d9a36d4b020d3
Agustin Carames <agustin.carames@creativecoefficient.net>
2020-02-06 15:01:17 -0300

    Remove stray debug code

--- src/src/store/selector/credentialSelector.ts
+++ src/src/store/selector/credentialSelector.ts
@@ -16,19 +16,6 @@ export const toplevelCredentialSelector = createSelector(
 		const nested = credentials.map(c => c.nested).reduce((acc, next) => acc.concat(next), []);
 		const res = credentials.filter(credential => !nested.find(nest => nest.jwt === credential.jwt));
 
-		res.push({
-			data: {
-				"1": "1"
-			},
-			issuer: EthrDID.fromKeyAddress("0x0123456789012345678901234567890123456789"),
-			jwt: "1223rqwe",
-			nested,
-			subject: EthrDID.fromKeyAddress("0x1234567890123456789012345678901234567890"),
-			title: "Share",
-			type: "CredentialDocument",
-			category: "finance"
-		});
-
 		return res.sort((l, r) => {
 			if (l.subject.did() !== r.subject.did()) {
 				if (l.subject.did() === did?.did?.()) {
