4b16e17a0669d62f47d61a15049b0141c88a2afd
Agustin Carames <agustin.carames@creativecoefficient.net>
2020-02-20 15:19:27 -0300

    Change where didi-sdk is located

--- src/src/AppConfig.ts
+++ src/src/AppConfig.ts
@@ -7,7 +7,7 @@ const debug: boolean = true;
 const defaultDebugServiceSettings: ServiceSettings = {
 	sharePrefix: "http://192.168.2.123:8080/api/credential_viewer",
 	trustGraphUri: "http://192.168.2.123:3001/graphql",
-	ethrDidUri: "https://mainnet.infura.io/v3/***REMOVED***",
+	ethrDidUri: "https://mainnet.infura.io/v3/21d2ee9de6484541b66eb06942ceb3c7",
 	ethrDelegateUri: "https://public-node.testnet.rsk.co:443",
 	didiUserServer: "http://192.168.2.123:3000/api/1.0/didi"
 };
@@ -15,7 +15,7 @@ const defaultDebugServiceSettings: ServiceSettings = {
 const defaultStagingServiceSettings: ServiceSettings = {
 	sharePrefix: "http://192.168.2.123:8080/api/credential_viewer",
 	trustGraphUri: "http://mou.nec.com.ar/graphql",
-	ethrDidUri: "https://mainnet.infura.io/v3/***REMOVED***",
+	ethrDidUri: "https://mainnet.infura.io/v3/21d2ee9de6484541b66eb06942ceb3c7",
 	ethrDelegateUri: "https://public-node.testnet.rsk.co:443",
 	didiUserServer: "http://didi.nec.com.ar:8089/api/1.0/didi"
 };
@@ -25,4 +25,4 @@ export const AppConfig: AppConfig = {
 	defaultServiceSettings: debug ? defaultDebugServiceSettings : defaultStagingServiceSettings
 };
 
-export const PRIVATE_KEY_SEED_PASSWORD = "***REMOVED***";
+export const PRIVATE_KEY_SEED_PASSWORD = "ffac4cd878a0ce0aa32c9e8181e23781";
