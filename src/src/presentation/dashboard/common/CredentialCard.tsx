56addc1998e2b8cf5af822a81ece5c036eb38303
Axel <abautista@atixlabs.com>
2020-06-22 14:03:49 -0300

    feat: added support in credential cards for custom layout and background image

--- src/src/presentation/dashboard/common/CredentialCard.tsx
+++ src/src/presentation/dashboard/common/CredentialCard.tsx
@@ -1,6 +1,6 @@
 import { ClaimDataPairs } from "didi-sdk";
 import React, { Component, Fragment } from "react";
-import { StyleSheet, Text, TextStyle, View, ViewStyle } from "react-native";
+import { StyleSheet, Text, TextStyle, View, ViewStyle, ImageBackground } from "react-native";
 
 import { DidiText } from "../../util/DidiText";
 
@@ -30,31 +30,72 @@ export default class CredentialCard extends Component<CredentialCardProps, {}> {
 		);
 	}
 
-	private renderKeyValuePairs(color: string) {
-		const columns = this.props.columns || 1;
+	private splitDataIntoRows() {
+		const { data, columns } = this.props;
+		const credFields = [...(data || [])];
 
-		const data = this.props.data || [];
-
-		const columnStyle: ViewStyle = {
-			flexDirection: columns === 1 ? "row" : "column",
-			width: `${100 / columns}%`
+		return [
+			{
+				data,
+				columns
+			}
+		];
+		// if layout
+		const layout = {
+			rows: [
+				{
+					columns: columns
+				}
+			]
 		};
-		const valueStyle: TextStyle =
-			columns === 1 ? { marginBottom: 0, textAlign: "right" } : { marginBottom: 5, textAlign: "left" };
-		return (
+
+		return layout.rows.map(row => {
+			return {
+				...row,
+				data: [...credFields.splice(0, row.columns)]
+			};
+		});
+	}
+
+	private renderKeyValuePairs(color: string) {
+		const rowData = this.splitDataIntoRows();
+
+		console.log("datax", JSON.stringify(rowData));
+
+		// const layout = {
+		// 	rows: [
+		// 		{
+		// 			columns: 3
+		// 		},
+		// 		{
+		// 			columns: 1
+		// 		}
+		// 	],
+		// 	backgroundImage: "http://localhost:3500/img/SancorSalud.png"
+		// };
+
+		const columnStyles: ViewStyle[] = rowData.map(row => ({
+			flexDirection: row.columns === 1 ? "row" : "column",
+			width: `${100 / row.columns}%`
+		}));
+		const valueStyle: TextStyle[] = rowData.map(row =>
+			row.columns === 1 ? { marginBottom: 0, textAlign: "right" } : { marginBottom: 5, textAlign: "left" }
+		);
+
+		return rowData.map((row, i) => (
 			<View style={styles.keyValueContainer}>
-				{data.map((item, index) => {
+				{row.data.map((item, index) => {
 					const effectiveValue = strings.credentialCard.formatValue(item.value);
 					return (
-						<View key={index} style={columnStyle}>
-							{data.length === 1 && item.label.length === 0 ? (
+						<View key={index} style={columnStyles[i]}>
+							{row.data.length === 1 && item.label.length === 0 ? (
 								<DidiText.Card.Value style={[styles.dataLabel, { color, textAlign: undefined }]}>
 									{effectiveValue}
 								</DidiText.Card.Value>
 							) : (
 								<Fragment>
 									<DidiText.Card.Key style={[styles.dataLabel, { color }]}>{item.label}</DidiText.Card.Key>
-									<DidiText.Card.Value style={[styles.dataValue, { color }, valueStyle]}>
+									<DidiText.Card.Value style={[styles.dataValue, { color }, valueStyle[i]]}>
 										{effectiveValue}
 									</DidiText.Card.Value>
 								</Fragment>
@@ -63,7 +104,7 @@ export default class CredentialCard extends Component<CredentialCardProps, {}> {
 					);
 				})}
 			</View>
-		);
+		));
 	}
 
 	render() {
