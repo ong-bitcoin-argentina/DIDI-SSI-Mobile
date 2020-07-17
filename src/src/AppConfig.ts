f43c47d7bc3102e73b0e2ed98c9493ac8f407345
Nicolas Laitan <nlaitan@atixlabs.com>
2020-07-17 14:52:39 -0300

    fix: add firebase functions

--- src/src/AppConfig.ts
+++ src/src/AppConfig.ts
@@ -18,13 +18,26 @@ const defaultDebugServiceSettings: ServiceSettings = {
 	}
 };
 
+// const defaultStagingServiceSettings: ServiceSettings = {
+// 	sharePrefix: "https://viewer.staging.didi.atixlabs.com/api/credential_viewer",
+// 	trustGraphUri: "https://mouro.staging.didi.atixlabs.com/graphql",
+// 	ethrDidUri: "https://did.testnet.rsk.co:4444",
+// 	ethrDelegateUri: "https://did.testnet.rsk.co:4444",
+// 	didiUserServer: "https://server.staging.didi.atixlabs.com/api/1.0/didi",
+// 	semillasServerUri: "https://api.staging.semillas.atixlabs.com",
+// 	semillasLogin: {
+// 		user: "didiUser@atixlabs.com",
+// 		password: "admin"
+// 	}
+// };
+
 const defaultStagingServiceSettings: ServiceSettings = {
-	sharePrefix: "https://viewer.staging.didi.atixlabs.com/api/credential_viewer",
-	trustGraphUri: "https://mouro.staging.didi.atixlabs.com/graphql",
+	sharePrefix: "http://alpha.didi.org.ar/api/credential_viewer",
+	trustGraphUri: "http:/alpha.didi.org.ar/graphql",
 	ethrDidUri: "https://did.testnet.rsk.co:4444",
 	ethrDelegateUri: "https://did.testnet.rsk.co:4444",
-	didiUserServer: "https://server.staging.didi.atixlabs.com/api/1.0/didi",
-	semillasServerUri: "https://api.staging.semillas.atixlabs.com",
+	didiUserServer: "http://alpha.didi.org.ar/api/1.0/didi",
+	semillasServerUri: "http://52.179.190.47",
 	semillasLogin: {
 		user: "didiUser@atixlabs.com",
 		password: "admin"
@@ -38,4 +51,4 @@ export const AppConfig: AppConfig = {
 
 export const PRIVATE_KEY_SEED_PASSWORD = Config.PRIVATE_KEY_SEED_PASSWORD;
 export const NOTIFICATION_SENDER_ID = Config.NOTIFICATION_SENDER_ID;
-export const PRESTADORES_FEATURE = (Config.PRESTADORES_FEATURE === 'true');
+export const PRESTADORES_FEATURE = Config.PRESTADORES_FEATURE === "true";
--- src/src/presentation/dashboard/semillas/SemillasScreen.tsx
+++ src/src/presentation/dashboard/semillas/SemillasScreen.tsx
@@ -24,6 +24,7 @@ import { PrestadoresProps } from "./PrestadoresScreen";
 import { SpecialCredentialMap } from "../../../store/selector/credentialSelector";
 import { haveEmailAndPhone } from "../../../util/specialCredentialsHelpers";
 import { ErrorDataAlert } from "../../common/ErrorDataAlert";
+import { logger } from "firebase-functions";
 
 export interface LoginScreenProps {}
 
@@ -94,6 +95,7 @@ class SemillasScreen extends NavigationEnabledComponent<
 				dni: cred.data.dni.toString()
 			});
 		}
+		logger.log("Entrando a pantalla semillas");
 	}
 
 	renderButton() {
