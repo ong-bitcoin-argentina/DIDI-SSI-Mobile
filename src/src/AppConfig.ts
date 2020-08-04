2e7dc288ae27381be2be4116e95981a043be496d
Nicolas Laitan <nlaitan@atixlabs.com>
2020-08-04 16:09:43 -0300

    fix: add command and gitignore

--- src/src/AppConfig.ts
+++ src/src/AppConfig.ts
@@ -6,37 +6,37 @@ type AppConfig = {
 	defaultServiceSettings: ServiceSettings;
 };
 
-const debug: boolean = true;
+const debug: boolean = Boolean(Config.APP_DEBUG);
 
-const defaultDebugServiceSettings: ServiceSettings = {
-	sharePrefix: "https://viewer.staging.didi.atixlabs.com/api/credential_viewer",
-	trustGraphUri: "https://mouro.staging.didi.atixlabs.com/graphql",
-	ethrDidUri: "https://did.testnet.rsk.co:4444",
-	ethrDelegateUri: "https://did.testnet.rsk.co:4444",
-	didiUserServer: "https://server.staging.didi.atixlabs.com/api/1.0/didi",
-	semillasServerUri: "https://api.staging.semillas.atixlabs.com",
-	semillasLogin: {
-		user: "didiUser@atixlabs.com",
-		password: "admin"
-	}
-};
-
-const defaultStagingServiceSettings: ServiceSettings = {
-	sharePrefix: "https://viewer.alpha.didi.org.ar/api/credential_viewer",
-	trustGraphUri: "https://mouro.alpha.didi.org.ar/graphql",
-	ethrDidUri: "https://did.testnet.rsk.co:4444",
-	ethrDelegateUri: "https://did.testnet.rsk.co:4444",
-	didiUserServer: "https://server.alpha.didi.org.ar/api/1.0/didi",
-	semillasServerUri: "https://api.semillas.didi.org.ar",
+const defaultServiceSettings: ServiceSettings = {
+	sharePrefix: Config.URL_VIEWER,
+	trustGraphUri: Config.URL_MOURO,
+	ethrDidUri: Config.URL_ETHR_DID,
+	ethrDelegateUri: Config.URL_ETHR_DELEGATOR,
+	didiUserServer: Config.URL_DIDI_SERVER,
+	semillasServerUri: Config.URL_SEMILLAS,
 	semillasLogin: {
 		user: Config.SEMILLAS_USERNAME,
 		password: Config.SEMILLAS_PASSWORD
 	}
 };
 
+// const defaultStagingServiceSettings: ServiceSettings = {
+// 	sharePrefix: "https://viewer.alpha.didi.org.ar/api/credential_viewer",
+// 	trustGraphUri: "https://mouro.alpha.didi.org.ar/graphql",
+// 	ethrDidUri: "https://did.testnet.rsk.co:4444",
+// 	ethrDelegateUri: "https://did.testnet.rsk.co:4444",
+// 	didiUserServer: "https://server.alpha.didi.org.ar/api/1.0/didi",
+// 	semillasServerUri: "https://api.semillas.didi.org.ar",
+// 	semillasLogin: {
+// 		user: SEMILLAS_USERNAME,
+// 		password: SEMILLAS_PASSWORD
+// 	}
+// };
+
 export const AppConfig: AppConfig = {
 	debug,
-	defaultServiceSettings: debug ? defaultDebugServiceSettings : defaultStagingServiceSettings
+	defaultServiceSettings
 };
 
 export const VERSION = Config.VERSION;
