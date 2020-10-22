c6b4378b563f088a4b470c2d945b3b506f891263
Nicolas Laitan <nlaitan@atixlabs.com>
2020-10-22 17:35:13 -0300

    fix: hide modal in actions

--- src/src/presentation/dashboard/home/Dashboard.tsx
+++ src/src/presentation/dashboard/home/Dashboard.tsx
@@ -87,12 +87,13 @@ class DashboardScreen extends NavigationEnabledComponent<
 	}
 
 	permissionDenied = async () => {
+		this.setState({ showModal: false });
 		await loginDenied();
 	};
 
 	permissionGranted = async () => {
 		const { did } = this.props;
-
+		this.setState({ showModal: false });
 		createToken(did).then(async (verification: string) => {
 			this.setState({ showModal: false }, () => successfullyLogged(verification));
 		});
