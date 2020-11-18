60fd7b5de6b998d7d3b23b7a074d92f4ac3115e3
Nicolas Laitan <nlaitan@atixlabs.com>
2020-11-18 17:57:28 -0300

    fix: restrict includes for extreme cases
    Signed-off-by: Nicolas Laitan <nlaitan@atixlabs.com>

--- src/src/util/utilHelpers.ts
+++ src/src/util/utilHelpers.ts
@@ -6,18 +6,20 @@ enum blockchains {
 	BFA = "BFA"
 }
 
-const customIncludes = (str1: string = "", str2: string = ""): boolean => {
+const insensitiveIncludes = (str1: string = "", str2: string = ""): boolean => {
 	return str1.toLowerCase().includes(str2.toLowerCase());
 };
+
+const wrap = (str: string) => `did:ethr:${str}:`;
 
 export const getBlockchain = (did: EthrDID): blockchains => {
 	const plainDid = did.did();
 	switch (true) {
-		case customIncludes(plainDid, blockchains.BFA):
+		case insensitiveIncludes(plainDid, wrap(blockchains.BFA)):
 			return blockchains.BFA;
-		case customIncludes(plainDid, blockchains.LACCHAIN):
+		case insensitiveIncludes(plainDid, wrap(blockchains.LACCHAIN)):
 			return blockchains.LACCHAIN;
-		case customIncludes(plainDid, blockchains.RSK):
+		case insensitiveIncludes(plainDid, wrap(blockchains.RSK)):
 			return blockchains.RSK;
 		default:
 			return blockchains.RSK;
