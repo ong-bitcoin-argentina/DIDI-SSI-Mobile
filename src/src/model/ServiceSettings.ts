a17a9b2bd1ed581478ee9f4c453df1b8443f72f8
Nicolas Laitan <nlaitan@atixlabs.com>
2020-11-26 14:54:49 -0300

    feat: take many providers and pass to parseJWT
    Signed-off-by: Nicolas Laitan <nlaitan@atixlabs.com>

--- src/src/model/ServiceSettings.ts
+++ src/src/model/ServiceSettings.ts
@@ -1,7 +1,10 @@
+import { ProviderConfig } from "didi-sdk";
+
 export interface ServiceSettings {
 	sharePrefix: string;
 	trustGraphUri: string;
 	ethrDidUri: string;
 	ethrDelegateUri: string;
 	didiUserServer: string;
+	providerConfig: ProviderConfig;
 }
