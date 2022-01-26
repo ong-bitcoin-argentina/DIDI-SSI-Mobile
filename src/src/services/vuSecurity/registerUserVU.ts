9e8eb9122cdbb8839479f3f9fbdf6a198b64118a
farpa <arpirez.fabio@gmail.com>
2022-01-26 13:23:04 -0300

    fix based on Function VUESecurity and its functionality

--- src/src/services/vuSecurity/registerUserVU.ts
+++ src/src/services/vuSecurity/registerUserVU.ts
@@ -2,10 +2,8 @@ import { VUSecurityApiClient} from '@proyecto-didi/app-sdk';
 import { AppConfig } from '../../AppConfig';
 
 
-function VUSecurity(): VUSecurityApiClient {
-    return new VUSecurityApiClient(AppConfig.defaultServiceSettings.urlSecurity);
-}
+const VUSecurity = ():VUSecurityApiClient => ( new VUSecurityApiClient(AppConfig.defaultServiceSettings.urlSecurity));
 
-export async function registerUserVU(did: string,name: string, lastname: string) {    
+export async function registerUserVU(did: string,name: string, lastname: string) { 
     return await VUSecurity().registerUser(did, name, lastname);
 }
