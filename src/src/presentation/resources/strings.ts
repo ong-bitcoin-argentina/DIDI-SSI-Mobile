114079161aaa94b0a4375e0290f2f5d9b9eb0fca
crc08 <52979332+crc08@users.noreply.github.com>
2022-02-12 15:36:08 +1000

    se elimina: Unused import DrawerLayoutAndroidBase
    Se atiende alerta de bot lgtm

--- src/src/presentation/resources/strings.ts
+++ src/src/presentation/resources/strings.ts
@@ -1,4 +1,4 @@
-import { DrawerLayoutAndroidBase, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { JWTParseError } from "@proyecto-didi/app-sdk";
 
 import { assertUnreachable } from "../../util/assertUnreachable";
