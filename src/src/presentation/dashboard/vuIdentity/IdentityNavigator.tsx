03f4204783afc1b0abba655722c6f2552eec6231
farpa <arpirez.fabio@gmail.com>
2022-02-16 12:42:08 -0300

    Added Renaper Navigation

--- src/src/presentation/dashboard/vuIdentity/IdentityNavigator.tsx
+++ src/src/presentation/dashboard/vuIdentity/IdentityNavigator.tsx
@@ -1,6 +1,7 @@
 import NavMap, { NavTree } from "../../util/NavMap";
 import { DashboardScreenProps } from '../home/Dashboard';
 import SemillasValidationScreen from '../semillas/validation/SemillasValidationScreen';
+import ValidateIdentityNavigator from '../validateIdentity/ValidateIdentityNavigator';
 import { IdentityScreen } from './IdentityScreen';
 import VuIdentityNavigator from './VuIdentityNavigator';
 
@@ -14,6 +15,7 @@ export interface IdentityNavigatorNavigation {
 export default function (then: NavTree<IdentityNavigatorNavigation>) {
 	return NavMap.from(IdentityScreen, {
 		...then,
+		ValidateID: ValidateIdentityNavigator,
 		VuIdentityID: VuIdentityNavigator,
 		ValidateSemillasID: NavMap.from(SemillasValidationScreen, {}),
 	});
