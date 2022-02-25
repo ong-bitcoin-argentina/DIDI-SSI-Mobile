48f55262ccb65a44ab722a97b39e55a360a4e953
farpa <arpirez.fabio@gmail.com>
2022-02-25 17:09:42 -0300

    added a new screen to navigation

--- src/src/presentation/dashboard/vuIdentity/VuIdentityNavigator.tsx
+++ src/src/presentation/dashboard/vuIdentity/VuIdentityNavigator.tsx
@@ -1,13 +1,14 @@
 import NavMap from "../../util/NavMap";
-import { VuIdentityBackScreen } from '../vuIdentity/VuIdentityBack';
-
-import { VuIdentityExplainHowScreen } from '../vuIdentity/VuIdentityExplainHow';
-import { VuIdentityFrontScreen} from '../vuIdentity/VuIdentityFront';
-
+import { VuIdentityBackScreen } from './VuIdentityBack';
+import { VuIdentityExplainHowScreen } from './VuIdentityExplainHow';
+import { VuIdentityFrontScreen} from './VuIdentityFront';
+import { VuIdentitySubmitScreen } from './VuIdentitySubmit';
 export default NavMap.from(VuIdentityExplainHowScreen, {
 	VuIdentityFront: NavMap.from(VuIdentityFrontScreen, {
 		VuIdentityBack: NavMap.from(VuIdentityBackScreen, {
+			VuIdentitySubmit: NavMap.from(VuIdentitySubmitScreen,{
 
+			})
 		})
 	})
 });
