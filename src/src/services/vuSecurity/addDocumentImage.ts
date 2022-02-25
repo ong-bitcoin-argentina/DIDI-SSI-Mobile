19543f1b9b0d018de912b87e35c5f72720d5d398
farpa <arpirez.fabio@gmail.com>
2022-02-25 17:05:18 -0300

    function created to add the back of the ID

--- src/src/services/vuSecurity/addDocumentImage.ts
+++ src/src/services/vuSecurity/addDocumentImage.ts
@@ -21,3 +21,22 @@ export async function addDocumentImageFront(
 	}
 	return result;
 }
+
+
+export async function addDocumentImageBack(
+	userName: string,
+	operationId: string,
+	file: string | undefined,
+	did: ActiveDid,
+	side = "back"
+): ApiResult<string>|{} {
+	let result;
+	try {
+		const token = await createTokenAuthorization(did);
+		return VUSecurity().addDocumentImage(userName, operationId, side, file, token);
+	} catch (error) {
+		result = { message: "Hubo un error al adherir la imagen documento" };
+	}
+	return result;
+}
+
