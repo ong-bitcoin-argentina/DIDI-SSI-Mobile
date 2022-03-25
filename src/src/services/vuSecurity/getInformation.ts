d61903ba6177de1655d3c5db3619df9e64cf0d49
farpa <arpirez.fabio@gmail.com>
2022-03-25 18:26:54 -0300

    getInformation function implementation

---
+++ src/src/services/vuSecurity/getInformation.ts
@@ -0,0 +1,13 @@
+import {VUSecurityApiClient} from '@proyecto-didi/app-sdk';
+import { AppConfig } from '../../AppConfig';
+import { IReturnGetInformation } from '../../model/VuGetInformation';
+import { createTokenAuthorization } from '../../presentation/util/appRouter';
+import { ActiveDid } from '../../store/reducers/didReducer';
+
+
+const VUSecurity = ():VUSecurityApiClient => ( new VUSecurityApiClient(AppConfig.defaultServiceSettings.urlSecurity));
+
+export async function getInformation(userName: string,operationId: string, did: ActiveDid ): Promise<IReturnGetInformation> {    
+    const token = await createTokenAuthorization(did);
+    return await VUSecurity().getInformation(userName, operationId,token);
+}
