f874399febf1af6782b410f0336c03917a786c24
farpa <arpirez.fabio@gmail.com>
2022-04-05 18:45:02 -0300

    cancelVerification connection solution

--- src/src/services/vuSecurity/cancelVerification.ts
+++ src/src/services/vuSecurity/cancelVerification.ts
@@ -9,5 +9,13 @@ const VUSecurity = ():VUSecurityApiClient => ( new VUSecurityApiClient(AppConfig
 
 export async function cancelVerificationVU(userName: string,operationId: string, did: ActiveDid ): Promise<IReturnCancel> {    
     const token = await createTokenAuthorization(did);
-    return VUSecurity().cancelVerification(userName, operationId,token);
+    try {
+    return await VUSecurity().cancelVerification(userName, operationId,token);   
+    } catch (error) {
+        return { 
+            status: "error",
+            errorCode: "CANCEL_OPERATION",
+            message: `${error}`,
+        };
+    }
 }
