c75fecdea57d0fed5e8c3995004cd094f86045fc
farpa <arpirez.fabio@gmail.com>
2022-04-29 19:41:06 -0300

    checkValidarDni function

---
+++ src/src/services/vuSecurity/checkValidateDni.ts
@@ -0,0 +1,25 @@
+import { VUSecurityApiClient} from '@proyecto-didi/app-sdk';
+import { AppConfig } from '../../AppConfig';
+import { IReturnCheckValidateDni } from '../../model/VuCheckValidateDni';
+import { createTokenAuthorization } from '../../presentation/util/appRouter';
+import { ActiveDid } from '../../store/reducers/didReducer';
+
+
+const VUSecurity = ():VUSecurityApiClient => ( new VUSecurityApiClient(AppConfig.defaultServiceSettings.urlSecurity));
+
+export async function checkValidateDniVU(did: ActiveDid): Promise<IReturnCheckValidateDni> {    
+    const token = await createTokenAuthorization(did);
+    try {
+    const result = await VUSecurity().checkValidateDni(`${did?.did()}`,token);  
+    return result; 
+    } catch (error) {
+        return { 
+            status: "error",
+            errorCode: "FIND_BY_ID",
+            message: `${error}`,
+            data: {
+                status: "Failed",
+            }
+        };
+    }
+}
