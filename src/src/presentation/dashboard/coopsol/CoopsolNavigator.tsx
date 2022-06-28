21bf047acd2b801bd269d005df186c492da092dd
farpires <arpirez.fabio@gmail.com>
2022-06-28 08:06:22 -0300

    navigation has been updated

--- src/src/presentation/dashboard/coopsol/CoopsolNavigator.tsx
+++ src/src/presentation/dashboard/coopsol/CoopsolNavigator.tsx
@@ -2,19 +2,19 @@ import NavMap, { NavTree } from "../../util/NavMap";
 
 import { DashboardScreenProps } from "../home/Dashboard";
 import VuIdentityNavigator from '../vuIdentity/VuIdentityNavigator';
-import { CoopsolScreen } from "./CoopsolScreen";
+import CoopsolScreen  from "./CoopsolScreen";
+import CoopsolValidationScreen from "./validation/CoopsolValidationScreen";
 
 export interface CoopsolNavigatorNavigation {
 	DashboardHome: DashboardScreenProps;
-	Prestadores: {};
-	Beneficiario: {};
 	ValidateID: {};
-	ValidateSemillasID: {};
+	ValidateCoopsolID: {};
 }
 
 export default function (then: NavTree<CoopsolNavigatorNavigation>) {
 	return NavMap.from(CoopsolScreen, {
 		...then,
-		ValidateID: VuIdentityNavigator
+		ValidateID: VuIdentityNavigator,
+		ValidateCoopsolID: NavMap.from(CoopsolValidationScreen, {}),
 	});
 }
