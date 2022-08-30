fadb5621c77c8ac3d9d2691b9ce6dcbbcf50bc16
alxrasa <alejandro.ramos@sw.com.mx>
2022-08-29 23:43:06 -0500

    add PoliticsTexts

---
+++ src/src/presentation/resources/PoliticsTexts.ts
@@ -0,0 +1,19 @@
+export default {
+    title: "Términos, Condiciones y Política de Privacidad",
+    description: "Antes de crear tu cuenta, queremos que sepas exactamente cómo funcionan los servicios de ai.di y por qué necesitamos tus datos. Revisa nuestros ",
+    description2: " que te ayudarán a continuar usando con tranquilidad y seguridad ai.di.",
+    data:[
+        {
+            title: "Responsable:",
+            description: "La Asociación Civil para el Desarrollo de Ecosistemas Descentralizados -DECODES-."
+        },
+        {
+            title: "Finalidades",
+            description: "Usamos tus datos personales, para crear una cuenta de usuario y acceder a los servicios de la aplicación móvil ai.di."
+        },
+        {
+            title: "Derechos",
+            description: "En cualquier momento cuentas con el derecho de acceder, rectificar y suprimir los datos, así como otros derechos."
+        }, 
+    ]
+}
